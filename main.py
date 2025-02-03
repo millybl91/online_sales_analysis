7225d6960492d5c394a033c87c66c491778cf858
Milan <millybl91@gmail.com>
2025-02-03 16:18:42 +0100

    Chaning the products inventory and deleting the execution of the same

--- main.py
+++ main.py
@@ -3,28 +3,12 @@ from product__manager import ProductManager
 
 def main():
     pm = ProductManager("", 0, 0)
-    pm1 = ProductManager("Dzemper", 500, 13)
-    pm2 = ProductManager("Patike", 800, 32)
-    pm3 = ProductManager("Cipele", 750, 14)
-    pm4 = ProductManager("Jakna", 630, 22)
+    pm1 = ProductManager("Majice", 240, 14)
+    pm2 = ProductManager("Odijela", 312, 30)
+    pm3 = ProductManager("Peskiri", 465, 15)
+    pm4 = ProductManager("Haljine", 726, 20)
 
-    pm.add_product(pm1)
-    pm.add_product(pm2)
-    pm.add_product(pm3)
-    pm.add_product(pm4)
-
-    pm.product_info_list()
-
-    pm.update_total_price(pm1.price)
-    pm.update_total_price(pm2.price)
-    pm.update_total_price(pm3.price)
-    pm.update_total_price(pm4.price)
-    print(f"Total product price from the list: {pm.total_price}")
-    print()
     
-    pm.remove_product("Jakna")
-    pm.product_info_list()
-    print(f"Total product price from the list: {pm.total_price}")
 
     
     
